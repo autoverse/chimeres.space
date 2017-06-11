080ac698214413a042508e25018e862844dfc5f5
Nikos Roussos <nikos@roussos.cc>
2017-06-11 16:12:09 +0300

    Create a more static space page

--- src/components/space.vue
+++ src/components/space.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>
-    <div id="map">
+  <div class="row">
+    <div class="col-sm-5">
+      <img src="../assets/img/painting.jpg" alt="building" class="img-fluid">
+    </div>
+    <div class="col-sm-7 details">
+      <div>
+        <i class="fa fa-map-marker" aria-hidden="true"></i> <strong>Τοποθεσία</strong><br>
+        <a href="http://osm.org/go/xxSitiXh9-?m=" target="_blank">Πυθοδώρου 6</a><br>
+        (1ος όροφος)<br>
+        Αθήνα (Βοτανικός)<br>
+        10444
+      </div>
+      <div>
+        <i class="fa fa-subway" aria-hidden="true"></i> <strong>Πρόσβαση</strong><br>
+        Μεταξουργείο (κόκκινη γραμμή)<br>
+        Καραμεικός (μπλε γραμμή)</span>)
+      </div>
+      <div>
+        <i class="fa fa-envelope" aria-hidden="true"></i> <strong>Επικοινωνία</strong><br>
+        <img src="https://chimeres.info/static/img/mail.png" alt="contact">
+      </div>
+      <div>
+        <a href="http://osm.org/go/xxSitiXh9-?m=" target="_blank"><img src="../assets/img/map.png" alt="map" class="img-fluid"></a>
+      </div>
     </div>
   </div>
 </template>
@@ -19,7 +41,7 @@
     },
 
     mounted () {
-      this.createMap()
+      // this.createMap()
     },
 
     methods: {
@@ -33,8 +55,6 @@
           center: [23.71560, 37.98584],
           zoom: 18
         });
-
-        //this.map.addControl(new mapboxgl.Navigation());
       }
     }
   }
@@ -44,5 +64,9 @@
   #map {
     width: 100%;
     height: 350px;
+  }
+
+  .details div {
+      margin-bottom: 20px;
   }
 </style>
