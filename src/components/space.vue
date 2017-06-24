6b8df455f894104ab182a4812b4124f0d3aa745a
Nikos Roussos <nikos@roussos.cc>
2017-06-24 15:59:29 +0300

    Add flickr slideshow
    * Add basic opengraph info

--- src/components/space.vue
+++ src/components/space.vue
@@ -1,27 +1,28 @@
 <template>
   <div class="row">
-    <div class="col-sm-5 hidden-xs-down">
-      <img src="../assets/img/painting.jpg" alt="building" class="img-fluid">
+    <div class="col-sm-4">
+      <div class="details ml-2">
+        <div>
+          <i class="fa fa-map-marker" aria-hidden="true"></i> <strong>Τοποθεσία</strong><br>
+          <a href="http://www.openstreetmap.org/way/494771197" target="_blank">Πυθοδώρου 6</a><br>
+          (1ος όροφος)<br>
+          Αθήνα (Βοτανικός)<br>
+          10444
+        </div>
+        <div>
+          <i class="fa fa-subway" aria-hidden="true"></i> <strong>Πρόσβαση</strong><br>
+          Μεταξουργείο (κόκκινη γραμμή)<br>
+          Καραμεικός (μπλε γραμμή)
+        </div>
+        <div>
+          <i class="fa fa-envelope" aria-hidden="true"></i> <strong>Επικοινωνία</strong><br>
+          <img src="https://chimeres.info/static/img/mail.png" alt="contact">
+        </div>
+      </div>
     </div>
-    <div class="col-sm-7 details">
-      <div>
-        <i class="fa fa-map-marker" aria-hidden="true"></i> <strong>Τοποθεσία</strong><br>
-        <a href="http://osm.org/go/xxSitiXh9-?m=" target="_blank">Πυθοδώρου 6</a><br>
-        (1ος όροφος)<br>
-        Αθήνα (Βοτανικός)<br>
-        10444
-      </div>
-      <div>
-        <i class="fa fa-subway" aria-hidden="true"></i> <strong>Πρόσβαση</strong><br>
-        Μεταξουργείο (κόκκινη γραμμή)<br>
-        Καραμεικός (μπλε γραμμή)
-      </div>
-      <div>
-        <i class="fa fa-envelope" aria-hidden="true"></i> <strong>Επικοινωνία</strong><br>
-        <img src="https://chimeres.info/static/img/mail.png" alt="contact">
-      </div>
-      <div>
-        <a href="http://osm.org/go/xxSitiXh9-?m=" target="_blank"><img src="../assets/img/map.png" alt="map" class="img-fluid"></a>
+    <div class="col-sm-8 ">
+      <div class="mr-2 text-right">
+        <a data-flickr-embed="true" href="https://www.flickr.com/photos/chimeresgr/albums/72157682366017022" title="Chimeres.Space"><img src="https://c1.staticflickr.com/5/4276/35267129542_34bd001c0e_z.jpg" width="100%" height="427" alt="Chimeres.Space"></a>
       </div>
     </div>
   </div>
@@ -59,13 +60,9 @@
     }
   }
 </script>
+<script src="../assets/js/flickr-client-code.js" charset="utf-8"></script>
 
 <style>
-  #map {
-    width: 100%;
-    height: 350px;
-  }
-
   .details div {
       margin-bottom: 20px;
   }
